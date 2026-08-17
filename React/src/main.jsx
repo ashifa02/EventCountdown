0af0eade830adceb1e2b6310b52e543149db663c
Ashifa <ashifanazar2@gmail.com>
2026-08-17 23:02:46 +0530

    Fix CSS import case for Vercel

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./AuthContext";
 import App from "./App.jsx";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
